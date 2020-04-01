a414f3e90d4623b7b01f3314677b453100f268a4
Luca Becker <luca.becker@me.com>
2020-04-01 21:41:48 +0200

    support for platforms, version bump 1.0.7

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import * as hap from 'hap-nodejs';
+import EventEmitter = NodeJS.EventEmitter;
+import Categories = HAPNodeJS.Accessory.Categories;
+import Service = HAPNodeJS.Service;
 
 type VersionNumber = number;
 type ServerVersionNumber = string;
 
-export interface HomebridgeApi {
+export interface HomebridgeApi extends EventEmitter {
     _accessories: object;
     _platforms: object;
     _configurableAccessories: object;
@@ -13,17 +16,22 @@ export interface HomebridgeApi {
 
     hap: typeof hap;
 
+    platformAccessory: HomebridgePlatformAccessory;
+
     publishCameraAccessories(pluginName: string, accessories: any): void;
 
     publishExternalAccessories(pluginName: string, accessories: any): void;
 
-    registerAccessory(pluginName: string, accessoryName: string, constructor: any, configurationRequestHandler?: any): void;
+    registerAccessory(pluginName: string,
+                      accessoryName: string,
+                      constructor: IHomebridgeAccessory,
+                      configurationRequestHandler?: any): void;
 
-    registerPlatform(pluginName: string, platformName: string, constructor: any, dynamic: any): void;
+    registerPlatform(pluginName: string, platformName: string, constructor: IHomebridgePlatform, dynamic: any): void;
 
-    registerPlatformAccessories(pluginName: string, platformName: string, accessories: any): void;
+    registerPlatformAccessories(pluginName: string, platformName: string, accessories: HomebridgePlatformAccessory[]): void;
 
-    unregisterPlatformAccessories(pluginName: string, platformName: string, accessories: any): void;
+    unregisterPlatformAccessories(pluginName: string, platformName: string, accessories: HomebridgePlatformAccessory[]): void;
 
     updatePlatformAccessories(accessories: any): void;
 }
@@ -36,11 +44,38 @@ export interface IAccessoryConfig {
     model?: string;
 }
 
+export interface IPlatformConfig {
+    platform: string;
+}
+
 export interface HomebridgeLogging {
     (...args: any[]): void;
+
     debug: (message?: any, ...optionalParams: any[]) => void;
     info: (message?: any, ...optionalParams: any[]) => void;
     error: (message?: any, ...optionalParams: any[]) => void;
+}
+
+export interface HomebridgePlatformAccessory {
+    new(displayName: string, UUID: string, category?: Categories): HomebridgePlatformAccessory;
+
+    addService(service: Service): Service;
+
+    removeService(service: Service): Service;
+
+    getService(name: string): Service;
+
+    getServiceByUUIDAndSubType(uuid: string, subtype: any): Service;
+
+    updateReachability(reachability: boolean): void;
+}
+
+export interface IHomebridgeAccessory {
+    new(log: HomebridgeLogging, config: object): IHomebridgeAccessory;
+}
+
+export interface IHomebridgePlatform {
+    new(log: HomebridgeLogging, config: object, api: HomebridgeApi): IHomebridgePlatform;
 }
 
 export abstract class HomebridgeAccessory {
@@ -53,4 +88,19 @@ export abstract class HomebridgeAccessory {
     public getServices(): (typeof hap.Service)[] {
         return this.services;
     }
+}
+
+export abstract class HomebridgePlatform {
+
+    protected readonly accessories: HomebridgeAccessory[] = [];
+
+    constructor(protected readonly log: HomebridgeLogging,
+                protected readonly config: IPlatformConfig,
+                protected readonly api: HomebridgeApi) {
+        this.api.on('didFinishLaunching', () => {
+            this.onHomebridgeDidFinishLaunching();
+        });
+    }
+
+    protected abstract onHomebridgeDidFinishLaunching(): void;
 }
